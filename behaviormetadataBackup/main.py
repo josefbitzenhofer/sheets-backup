80b934ee5f5e4387c6784ecc181c27c0f6da0d9c
James Rowland <jrowlandm@gmail.com>
2024-07-16 16:22:49 +0100

    Large scale improvements, working on server (see subcommits for details) (#3)
    * v.2.1
    
    * v2.2
    
    * Delete token.pickle
    
    * Update README.md
    
    * Update README.md
    
    * add .gitignore file
    
    * delete __pycache__
    
    * Implemented feedback
    
    * Resolved typo
    
    * Improved feedback: Got rid of unnecessary hash lines, improved error handling
    
    * added a break if attibute error
    
    ---------
    
    Co-authored-by: josefbitzenhofer <josef.bitzenhofer@icloud.com>
    Co-authored-by: josefbitzenhofer <167422231+josefbitzenhofer@users.noreply.github.com>

--- behaviormetadataBackup/main.py
+++ behaviormetadataBackup/main.py
@@ -1,74 +1,150 @@
-import gspread
-import pandas as pd
 import sys
 from datetime import date
 from datetime import datetime
 import os
 import yaml
+from gsheets_importer import gsheet2df, list_worksheets
 
-##################################################################
-# Opening the Google Spreadsheet
-def open_spreadsheet(sh_title):            
-    gc = gspread.oauth()  # Authenticating with the created credentials
-    sh = gc.open(sh_title)  # Opening the Google spreadsheet
-    return sh
 
-##################################################################
-# Getting the timestamp
-def get_timestamp():
-    # JB: get the current date
+def get_timestamp() -> str:
+    """ "Return timestamp"""
+    # Get the current date
     today = date.today()
     today = today.strftime("%Y-%m-%d")
-    # JB: get the current time
+    # Get the current time
     time = datetime.now()
     time = time.strftime("%H-%M-%S")
 
-    # JB: Create timestamp
-    timestamp = today + "_" + time  # JB: 'YYYY-MM-DD_HH-MM-SS'
+    # Create timestamp
+    timestamp = today + "_" + time  # 'YYYY-MM-DD_HH-MM-SS'
     return timestamp
 
 
-##################################################################
-# Creating a folder for each day
-def create_directory(parent_directory):
-    new_directory = (
-        get_timestamp()
-    )  # JB: This sets the name of the new folder to the timestamp
+def create_todays_directory(parent_directory: str) -> str:
+    """
+    Return today's directory
+    Args:
+        parent_directory (str):         Parent directory path (aka umbrella folder for each day's backup folder)
+    Returns:
+        str: Today's directory path
+    """
+    new_directory = get_timestamp()
+    # This sets the name of the new folder to the timestamp
     directory_path = os.path.join(parent_directory, new_directory)
     os.makedirs(directory_path)
-    print("NOTE: Created new directory @", directory_path)
+    print(f"NOTE: Created new directory @ '{directory_path}'.")
     return directory_path
 
-##################################################################
-# Creating .csv files
-def create_csv(sh, parent_directory):
-    worksheet_list = sh.worksheets()                        # Getting a list of worksheets we can iterate over
-    directory_path = create_directory(parent_directory)
-    for worksheet in worksheet_list:
-        worksheet_df = pd.DataFrame(
-            worksheet.get_all_values()
-        )  # Getting all data of the worksheet into a Pandas dataframe
-        # Setting the filename
-        file_name = worksheet.title + ".csv"
+
+def create_csv(
+    spreadsheet_id: str,
+    spreadsheet_title: str,
+    parent_directory: str,
+    header_row: int,
+    all_worksheets: bool,
+    worksheets_user: list,
+) -> None:
+    """
+    Create folder and CSV files.
+    Arg:
+        spreadsheet_id (str):           Spreadsheet id
+        spreadsheet_title (str):        Spreadsheet title
+        parent_directory (str):         Parent directory path (aka umbrella folder for each day's backup folder)
+        header_row (int):               Row number of the row with the headers in the worksheets
+        all_worksheets (bool):          Either 'True' or 'False' -> if 'True', save all worksheets within a spreadsheet
+        worksheets_user (list):         If all_worksheets 'False', take list of worksheets within a spreadsheet to be saved
+    Returns:
+        None
+    Raises:
+        AttributeError:                 If gsheet2df returns 'None'
+    """
+    directory_path = create_todays_directory(parent_directory)
+    # This checks if the user wants all worksheets within the spreadsheet to be saved. Else, it takes the worksheets the user specified.
+    worksheets = []
+    if all_worksheets:
+        worksheets = list_worksheets(spreadsheet_id)
+    else:
+        worksheet_names = worksheets_user
+        for worksheet_name in worksheet_names:
+            # If the sheet name contains numbers, it must be enclosed in single quotes # JB: We have to deal with blank spaces as well.
+            if any(char.isdigit() for char in worksheet_name) or any(
+                char.isspace() for char in worksheet_name
+            ):
+                worksheet_name = f"'{worksheet_name}'"
+                worksheets.append(worksheet_name)
+    for worksheet in worksheets:
+        sheet_name = worksheet
+        try:
+            sheet_df = gsheet2df(spreadsheet_id, sheet_name, header_row)
+        except AttributeError as e:
+            print(
+                f"ERROR: ExceptionError: The worksheet '{sheet_name}' appears to be empty. Exception: '{e}'."
+            )
+            break
+        file_name = sheet_name + ".csv"
         file_path = os.path.join(directory_path, file_name)
-        # Dumping the Pandas dataframe into a .csv file
-        worksheet_df.to_csv(file_path)
-    print("NOTE: Backup of", sh_title, "@", directory_path, "successful.")
+        sheet_df.to_csv(file_path)
 
-##################################################################
-# Main code
-with open("config.yaml", "r") as file:
-        # Getting the variables from the config.yaml file
+    print(f"NOTE: Backup of '{spreadsheet_title}' @ '{directory_path}' successful.")
+
+
+def main():
+    """
+    Get config from 'config.yaml' file.
+    Iterate over each worksheet to be saved, turn it into .csv file.
+    Returns:
+        None
+    Raises:
+        FileNotFoundError:              If the parent directory does not exists
+        ValueError:                     If a value within the 'config.yaml' file has not properly been set up
+    """
+    with open("config.yaml", "r") as file:
         spreadsheets = yaml.safe_load_all(file)
-
         for spreadsheet in spreadsheets:
-            # Get the variables for every spreasheet
-            sh_title = spreadsheet["sh_title"]
-            parent_directory = spreadsheet["parent_directory"]
-            # Then open spreadsheet and create the backup
-            sh = open_spreadsheet(sh_title)
-            create_csv(sh, parent_directory)
+            try:
+                spreadsheet_title = spreadsheet["spreadsheet_title"]
+                active = spreadsheet["active"]
+                if active:
+                    print(f"NOTE: Starting backup for '{spreadsheet_title}'.")
+                    spreadsheet_id = spreadsheet["spreadsheet_id"]
+                    parent_directory = spreadsheet["parent_directory"]
+                    if not os.path.isdir(parent_directory):
+                        raise FileNotFoundError(
+                            f"ERROR: The directory '{parent_directory}' does not exist."
+                        )
+                    header_row = spreadsheet["header_row"]
+                    all_worksheets = spreadsheet["all_worksheets"]
+                    if not all_worksheets:
+                        worksheets_user = spreadsheet["worksheets"]
+                        create_csv(
+                            spreadsheet_id,
+                            spreadsheet_title,
+                            parent_directory,
+                            header_row,
+                            all_worksheets,
+                            worksheets_user,
+                        )
+                    else:
+                        worksheets_user = []
+                        create_csv(
+                            spreadsheet_id,
+                            spreadsheet_title,
+                            parent_directory,
+                            header_row,
+                            all_worksheets,
+                            worksheets_user,
+                        )
+                else:
+                    print(
+                        f"NOTE: No backup for '{spreadsheet_title}'. `active` is set to `{active}`."
+                    )
+            except Exception as e:
+                raise ValueError(
+                    f"ERROR: ExceptionError: You did not correctly set up your 'config.yaml' file for '{spreadsheet_title}.'"
+                ) from e
 
-# Exit
-print("NOTE: Graceful exit")
-sys.exit()
+
+if __name__ == "__main__":
+    main()
+    print("NOTE: Graceful exit.")
+    sys.exit()
